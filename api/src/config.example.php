0242a7d550794a306ba4b19faad9fbb3228ceae4
Maarten v/d Heijden <32467485+survivorbat@users.noreply.github.com>
2018-02-09 13:12:00 +0100

    Update config.example.php

--- api/src/config.example.php
+++ api/src/config.example.php
@@ -1,3 +1,3 @@
-define('DB_HOST',"mysql:host=localhost;dbname=digituintestdb;charset=utf8mb4"); //Hostlink
+define('DB_HOST',"mysql:host=localhost;dbname=messagevoid;charset=utf8mb4"); //Hostlink
 define('DB_USER',"root"); //Database user
 define('DB_PASS',""); //database password
